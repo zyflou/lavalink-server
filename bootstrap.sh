dd9d2c280d3721f3298437d3197dd389a54c4137
cobaltgit <criterion@chitco.co.uk>
2022-02-07 15:23:44 +0000

    Example YML

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,7 @@ echo "Downloading envsubst binary..."
 curl -fsSL "https://github.com/a8m/envsubst/releases/download/v1.2.0/envsubst-$(uname -s)-$(uname -m)" -o envsubst
 chmod +x envsubst
 echo "Substituting environment variables..."
-./envsubst < application.yml > application.yml.tmp && mv application.yml.tmp application.yml
+./envsubst < application.example.yml > application.yml
 rm -f envsubst
 
 echo "All set! Now, let's start Lavalink!"
