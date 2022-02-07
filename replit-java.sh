a2e5ca4c3b84ca163b38c041d02470bc71cf5d92
cobaltgit <criterion@chitco.co.uk>
2022-02-07 18:32:41 +0000

    Use Azul Zulu 17

--- replit-java.sh
+++ replit-java.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # Replit Java Bootstrapper
 
+OPENJDK_VERSION=zulu@1.17.0-0
+
+echo "Installing JDK $OPENJDK_VERSION..."
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash -s -- --skip-rc && . ~/.jabba/jabba.sh
-jabba install openjdk@1.13.0-2
+jabba install $OPENJDK_VERSION
 
 exec bash bootstrap.sh
